9be350b16fbf15e035527ec90471907a251f9b6d
Vahini <vahiniprasad@users.noreply.github.com>
2014-04-27 08:08:36 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,49 +2,46 @@
 ## functions do
 
 
-## This function comprises  four functions set  matrix,get  matrix,set  inverse of  matrix, 
-## get inversed matrix
+## This function returns four functions set, get, set inverse and get inverse
+## of given matrix
 
 makeCacheMatrix <- function(x = matrix()) {
     ## initializing inverse matrix to NULL
-    inverse_x <- NULL
+    inv <- NULL
     ## set function to cache matrix 
     set <- function(y) {
         x <<- y
-        inverse_x <<- NULL
+        inv <<- NULL
     }
     ## get function to retrive matrix from cache
     get <- function() x
     ## setinv function computes inverse matrix & caches it
     ## uses solve function to compute matrix inverse
-    setinv <- function(solve) inverse_x <<- solve
-    ## getinv function to retrive inverse matrix from cache
-    getinv <- function() inverse_x
+    setinv <- function(solve) inv <<- solve
+    ## getinv function retrives inverse matrix from cache
+    getinv <- function() inv
     ## The Four functions of makeCacheMatrix
     list(set = set, get = get, setinv = setinv, 
          getinv = getinv)
 }
 
 
-## cacheSolve computes, retrives and sets inverse matrix
-
-
+## cacheSolve retrives or omputes & sets inverse matrix
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    
+    inv <- x$getinv()
     ## checks if inverse is already computed
-    if(!is.null(inverse_x)) {
+    if(!is.null(inv)) {
         message("getting cached data")
         ## return the cached inverse matrix
-        return(inverse_x)
+        return(inv)
     }
     
-    ## if inverse matrix in not already computed then
-    ## get the cached matrix 
+    ## if inverse matrix in not already computed then get cached matrix 
     data <- x$get()
     ## compute the inverse matrix using solve function
-    inverse_x <- solve(data, ...)
+    inv <- solve(data, ...)
     ## set the inverse matrix in cache 
-    x$setinv(inverse_x)
-    inverse_x
+    x$setinv(inv)
+    inv
 }
